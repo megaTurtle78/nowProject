71edbe170bd9cd9aeed21c73a4b58b2e1aeb8e2a
내 이름 <c19st23@pager.kr>
2024-03-28 18:10:05 +0900

    시간 조작 만드는 중

--- cardMatchProject/build/script/cardMatch.js
+++ cardMatchProject/build/script/cardMatch.js
@@ -40,7 +40,7 @@
             //
             // 카드 게임 로직 시작
             this.nowStage.init();
-            this.nowStage.visualizeCardList();
+            this.nowStage.gameControl();
             //
         }
         loadCardList() {
@@ -73,7 +73,7 @@
             this.amountCard = findCardFriend.defaultCardAmount + ((this.level - 1) * findCardFriend.cardIncrease);
             this.remainCard = findCardFriend.defaultCardAmount + ((this.level - 1) * findCardFriend.cardIncrease);
             this.remainTime = findCardFriend.readyTime;
-            this.randomCardArrangement = [];
+            this.randomCardOrder = [];
             this.correctAnswer = [];
             this.gameStateArray = [];
             this.clickFlag = false;
@@ -96,7 +96,7 @@
                     this.gameStateArray.push([]);
                     this.correctAnswer.push([]);
                     for (let j = 0; j < colValue; j++) {
-                        this.gameStateArray[i].push(0);
+                        this.gameStateArray[i].push(1);
                         this.correctAnswer[i].push(0);
                     }
                 }
@@ -104,34 +104,35 @@
             //
             // 랜덤으로 게임에 사용할 카드 리스트 만들기
             const makeRandomCardList = () => {
-                let randomNumber = 0;
+                // tempArray에 랜덤카드 4장을 뽑아준다. 
                 let tempArray = [];
-                let randomOrder = [];
-                // 랜덤 숫자 생성햬서 tempArray에 담는다
+                let tempOrder = [];
+                while (tempArray.length < this.amountCard / 2) {
+                    let randomNumber = Math.floor(Math.random() * findCardFriend.cardList.length);
+                    if (!tempArray.includes(findCardFriend.cardList[randomNumber])) {
+                        tempArray.push(findCardFriend.cardList[randomNumber]);
+                    }
+                }
+                console.log("tempArray", tempArray);
+                // tempOrder에 랜덤숫자 8개를 뽑아준다.
+                while (tempOrder.length < this.amountCard) {
+                    let randomNumber = Math.floor(Math.random() * this.amountCard);
+                    if (!tempOrder.includes(randomNumber)) {
+                        tempOrder.push(randomNumber);
+                    }
+                }
+                console.log("tempOrder", tempOrder);
+                // this.randomCardOrder에 tempOrder를 가져와서 tempArray에 있는 카드를 넣어준다. 한 번의 두 개씩.
                 for (let i = 0; i < this.amountCard / 2; i++) {
-                    do {
-                        randomNumber = Math.floor(Math.random() * findCardFriend.cardList.length);
-                    } while (tempArray.includes(findCardFriend.cardList[randomNumber]))
-                    tempArray[i] = findCardFriend.cardList[randomNumber];
+                    this.randomCardOrder[tempOrder[i]] = tempArray[i].name;
+                    this.randomCardOrder[tempOrder[i + (this.amountCard / 2)]] = tempArray[i].name;
                 }
-                for (let i = 0; i < this.amountCard; i++) {
-                    do {
-                        randomNumber = Math.floor(Math.random() * this.amountCard);
-                    } while (randomOrder.includes(randomNumber))
-                    randomOrder[i] = randomNumber;
-                }
-                for (let i = 0; i < this.amountCard / 2; i++) {
-                    this.randomCardArrangement[randomOrder[i]] = tempArray[i].name;
-                    this.randomCardArrangement[randomOrder[i + (this.remainCard / 2)]] = tempArray[i].name;
-                }
-                for (var i = 0; i < this.randomCardArrangement.length; i++) {
+                console.log("randomCardOrder", this.randomCardOrder);
+                // this.correctAnswer 만들기(정복 X)
+                for (let i = 0; i < this.randomCardOrder.length; i++) {
                     let rowValue = Math.floor(i / this.gameStateArray[0].length);
                     let colValue = i % this.gameStateArray[0].length;
-
-                    if (this.gameStateArray.length <= rowValue) {
-                        this.gameStateArray.push([]);
-                    }
-                    this.correctAnswer[rowValue][colValue] = this.randomCardArrangement[i];
+                    this.correctAnswer[rowValue][colValue] = this.randomCardOrder[i];
                 }
                 console.log("게임상태", this.gameStateArray);
                 console.log("게임정답지", this.correctAnswer);
@@ -144,57 +145,93 @@
             document.getElementById("currentScore").innerHTML = findCardFriend.scoreAmount;
             document.getElementById("currentRemainTime").innerHTML = findCardFriend.formatTime(this.remainTime);
         }
-        // 카드 시각화
-        visualizeCardList = () => {
-            const giveRandomUrl = () => {
-                const nodeListArray = document.querySelectorAll(`.cardElementCol`);
-                for (let i = 0; i < this.gameStateArray.length; i++) {
-                    for (let j = 0; j < this.gameStateArray[0].length; j++) {
-                        if (nodeListArray[i].classList.contains("nowChoicedCard")) {
-                            nodeListArray[i].style.backgroundImage = `url('../res/cardImage/${this.cardArrangeArray[i][j]}.png')`;
+        // 게임 컨트롤 관련 함수들
+        gameControl = () => {
+            const startReadyTime = () => {
+                const nowTimeInfo = new Date();
+                const endTimeInfo = nowTimeInfo.getTime() + (this.remainTime * 1000);
+                const checkEndReadyTime = () => {
+                    console.log("dajlfdkijflkj");
+                    if (nowTimeInfo.getTime() > endTimeInfo) {
+                        for (let i = 0; i < this.gameStateArray.length; i++) {
+                            for (j = 0; i < this.gameStateArray[0].length; j++) {
+                                this.gameStateArray[i][j] = 0;
+                                // this.gameControl();
+                                // this.remainTime = findCardFriend.timeLimit;
+                                // document.getElementById("currentRemainTime").innerHTML = cardMatchGame.formatTime(this.remainTime);
+                                // clearInterval(checkEndReadyTime);
+                            }
                         }
+                    }
+                    this.remainTime--;
+                    document.getElementById('currentRemainTime').innerHTML = findCardFriend.formatTime(this.remainTime);
+                }
+                setInterval(checkEndReadyTime(), 1000);
+            }
+            const resetGameCard = () => {
+                while (findCardFriend.cardDisplayDom.firstChild) {
+                    findCardFriend.cardDisplayDom.removeChild(findCardFriend.cardDisplayDom.firstChild);
+                }
+            }
+            const visualizeCardList = () => {
+                let newRowDiv = null;
+                let newColDiv = null;
+                for (let i = 0; i < this.gameStateArray.length; i++) {
+                    newRowDiv = document.createElement('div');
+                    newRowDiv.classList.add("cardElementRow");
+                    findCardFriend.cardDisplayDom.appendChild(newRowDiv);
+                    for (let j = 0; j < this.gameStateArray[0].length; j++) {
+                        newColDiv = document.createElement('div');
+                        newColDiv.id = `${i}_${j}`;
+                        newColDiv.classList.add("cardElementCol");
+                        switch (this.gameStateArray[i][j]) {
+                            case 0:
+                                newColDiv.classList.add("misteryCard");
+                                break;
+                            case 1:
+                                newColDiv.classList.add("nowChoicedCard");
+                                break;
+                            case 2:
+                                newColDiv.classList.add("foundCard");
+                                break;
+                        }
+                        newRowDiv.appendChild(newColDiv);
+                    }
+                }
+            }
+            const giveRandomUrl = () => {
+                const cardNodeList = document.querySelectorAll('.cardElementCol');
+                for (let i = 0; i < this.amountCard; i++) {
+                    if (cardNodeList[i].classList.contains("nowChoicedCard")) {
+                        cardNodeList[i].style.backgroundImage = `url('../res/cardImage/${this.randomCardOrder[i]}.png')`;
                     }
                 }
             }
             const giveCardClickEvent = () => {
-                document.querySelectorAll('.cardElementCol').forEach(v => {
+                const cardNodeList = document.querySelectorAll('.cardElementCol');
+                cardNodeList.forEach(v => {
                     v.addEventListener("click", e => {
                         const [rowIndex, colIndex] = e.target.id.split('_');
                         this.gameStateArray[Number(rowIndex)][Number(colIndex)] = 1;
-                        // this.clickFlag 변경 필요
-                        this.visualizeCardList();
+                        this.gameControl();
+                    });
+                })
+            }
+            const changeClickFlag = () => {
+                const cardNodeList = document.querySelectorAll('.cardElementCol');
+                cardNodeList.forEach(v => {
+                    v.addEventListener("click", e => {
+                        this.clickFlag = !this.clickFlag;
+                        console.log(this.clickFlag)
                     });
                 });
             }
-            while (findCardFriend.cardDisplayDom.firstChild) {
-                findCardFriend.cardDisplayDom.removeChild(findCardFriend.cardDisplayDom.firstChild);
-            }
-            let newRowDiv = null;
-            let newColDiv = null;
-            for (let i = 0; i < this.gameStateArray.length; i++) {
-                newRowDiv = document.createElement('div');
-                newRowDiv.classList.add("cardElementRow");
-                findCardFriend.cardDisplayDom.appendChild(newRowDiv);
-                for (let j = 0; j < this.gameStateArray[0].length; j++) {
-                    newColDiv = document.createElement('div');
-                    newColDiv.id = `${i}_${j}`;
-                    newColDiv.classList.add("cardElementCol");
-                    switch (this.gameStateArray[i][j]) {
-                        case 0:
-                            newColDiv.classList.add("misteryCard");
-                            break;
-                        case 1:
-                            newColDiv.classList.add("nowChoicedCard");
-                            break;
-                        case 2:
-                            newColDiv.classList.add("foundCard");
-                            break;
-                    }
-                    newRowDiv.appendChild(newColDiv);
-                }
-            }
+            resetGameCard();
+            visualizeCardList();
             giveRandomUrl();
             giveCardClickEvent();
+            changeClickFlag();
+            startReadyTime();
         }
     }
 
